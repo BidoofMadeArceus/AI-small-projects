6f2007610bc3f1f868f38107eca475337d9c9b5c
A Karim <adamkhalilkarim@gmail.com>
2026-06-20 01:22:02 +0100

    Update RainChanceModel.py

--- RainChance/RainChanceModel.py
+++ RainChance/RainChanceModel.py
@@ -1,1 +1,19 @@
+import numpy as np
+from math import e
 
+def train(X, y, w, b, l):
+  for loop in range(1000):
+    wErrors = []
+    bErrors = []
+    for i in range(len(X)):
+      z = b + (w * X[i])
+      predict = 1 / (1 + (e ** (0 - z)))
+      wError = (predict - y[i]) * X[i]
+      bError = (predict - y[i])
+      wErrors.append(wError)
+      bErrors.append(bError)
+    wSlope = (sum(wErrors)) / len(X)
+    bSlope = (sum(bErrors)) / len(X)
+    w -= (wSlope * l)
+    b -= (bSlope * l)
+  return w,b
