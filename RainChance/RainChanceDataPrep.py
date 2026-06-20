f0ee2be4cbeb665650999fa5036f8ae848e8a88b
A Karim <adamkhalilkarim@gmail.com>
2026-06-20 19:29:24 +0100

    Update RainChanceDataPrep.py

--- RainChance/RainChanceDataPrep.py
+++ RainChance/RainChanceDataPrep.py
@@ -1,6 +1,13 @@
 import pandas as pd
+from sklearn.model_selection import train_test_split
 
 df = pd.read_csv("RainChanceData.csv")
 df = df.dropna(subset=["PRCP"])
+df = df.dropna(subset=["TMAX"])
+df = df.dropna(subset=["TMIN"])
 
 df["Rain"] = (df["PRCP"] > 0).astype(int)
+
+X1 = df["TMAX"]
+X2 = df["TMIN"]
+y = df["Rain"]
