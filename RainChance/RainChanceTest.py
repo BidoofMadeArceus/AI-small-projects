85c83590635ccb7e1e56cf84587a77898718dde3
A Karim <adamkhalilkarim@gmail.com>
2026-06-21 09:29:15 +0100

    Update RainChanceTest.py

--- RainChance/RainChanceTest.py
+++ RainChance/RainChanceTest.py
@@ -1,18 +1,25 @@
-import pandas as pd
 from math import e
 import numpy as np
 from RainChanceDataPrep import X1_test
 from RainChanceDataPrep import X2_test
 from RainChanceDataPrep import y_test
+from RainChanceTrainV1 import weight_1
+from RainChanceTrainV1 import weight_2
+from RainChanceTrainV1 import bias
 
 errors = []
 
 for i in range(len(X1_test)):
   z = (X1_test.iloc[i] * weight_1) + (X2_test.iloc[i] * weight_2) + bias
   predict = 1 / (1 + e ** (-z))
-  error = (y_test[i].iloc * np.log10(predict)) + ((1 - y_test[i].iloc) * np.log10(1 - predict))
+  error = (y_test.iloc[i] * np.log(predict)) + ((1 - y_test.iloc[i]) * np.log(1 - predict))
   errors.append(error)
 
 LogLoss = (-1) * (sum(errors) / len(X1_test))
 
 print("LogLoss: " + str(LogLoss))
+
+for i in range(10):
+    z = (X1_test.iloc[i] * weight_1) + (X2_test.iloc[i] * weight_2) + bias
+    p = 1 / (1 + e**(-z))
+    print(f"z={z:.3f}, p={p:.6f}, y={y_test.iloc[i]}")
