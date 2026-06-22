Fix calculation of z in RainChanceTest.py

RainChance/RainChanceTest.py
@@ -2,7 +2,6 @@ from math import e
 import numpy as np
 from RainChanceDataPrep import X1_test
 from RainChanceDataPrep import X2_test
-from RainChanceDataPrep import X3_test
 from RainChanceDataPrep import y_test
 from RainChanceTrain import weight_1
 from RainChanceTrain import weight_2
@@ -11,7 +10,7 @@ from RainChanceTrain import bias
 errors = []
 
 for i in range(len(X1_test)):
-  z = (X1_test.iloc[i] * weight_1) + (X2_test.iloc[i] * weight_2) + (weight_3 * X3_train.iloc[i]) + bias
+  z = (X1_test.iloc[i] * weight_1) + (X2_test.iloc[i] * weight_2)) + bias
   predict = 1 / (1 + e ** (-z))
   error = (y_test.iloc[i] * np.log(predict)) + ((1 - y_test.iloc[i]) * np.log(1 - predict))
   errors.append(error)
@@ -21,6 +20,6 @@ LogLoss = (-1) * (sum(errors) / len(X1_test))
 print("LogLoss: " + str(LogLoss))
 
 for i in range(10):
-    z = (X1_test.iloc[i] * weight_1) + (X2_test.iloc[i] * weight_2) + (weight_3 * X3_train.iloc[i]) + bias
+    z = (X1_test.iloc[i] * weight_1) + (X2_test.iloc[i] * weight_2)) + bias
     p = 1 / (1 + e**(-z))
-    print(f"z={z:.3f}, p={p:.6f}, y={y_test.iloc[i]}")
+    print("z= " + str(z) + " " + "p= " + str(p) + " " + "y= " + str(y_test.iloc[i]))