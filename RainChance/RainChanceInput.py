305ae0abb2b0e8e3793f84a248292be3c3050aba
A Karim <158174701+UnsurpassedRobot@users.noreply.github.com>
2026-06-22 15:26:24 +0100

    Add normalization for temperature range and average
    Normalize temperature inputs using X1 data.

--- RainChance/RainChanceInput.py
+++ RainChance/RainChanceInput.py
@@ -1,10 +1,15 @@
 from RainChanceTrain import weight_1
 from RainChanceTrain import weight_2
 from RainChanceTrain import bias
+from RainChanceDataPrep import X1
+from RainChanceDataPrep import X1
 from math import e
 
 TempRange = int(input("temperature range: "))
 AvgTemp = int(input("average temperature: "))
+
+TempRange = (TempRange - X1.min())/(X1.max() - X1.min())
+TempRange = (AvgTemp - X1.min())/(X1.max() - X1.min())
 
 z = bias + (weight_1 * TempRange) + (weight_2 * AvgTemp)
 predict = 1 / (1 + e ** (0-z))
