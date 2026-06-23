a57fd0d20ec2c443910bee17f4351f6434ab588a
A Karim <158174701+UnsurpassedRobot@users.noreply.github.com>
2026-06-23 14:58:23 +0100

    Add latitude and longitude to rain chance prediction

--- RainChance/RainChanceInput.py
+++ RainChance/RainChanceInput.py
@@ -1,21 +1,27 @@
 from RainChanceTrain import weight_1
 from RainChanceTrain import weight_2
 from RainChanceTrain import weight_3
+from RainChanceTrain import weight_4
+from RainChanceTrain import weight_5
 from RainChanceTrain import bias
 from RainChanceDataPrep import Scale1
 from RainChanceDataPrep import Scale2
 from RainChanceDataPrep import Scale3
+from RainChanceDataPrep import Scale4
+from RainChanceDataPrep import Scale5
 from math import e
 
 TempRange = int(input("temperature range: "))
 AvgTemp = int(input("average temperature: "))
 Elevation = int(input("elevation: "))
+Latitude = int(input("latitude: "))
+Longitude = int(input("longitude: "))
 
 TempRange = (TempRange - Scale1.min())/(Scale1.max() - Scale1.min())
 AvgTemp = (AvgTemp - Scale2.min())/(Scale2.max() - Scale2.min())
 Elevation = (Elevation - Scale3.min())/(Scale3.max() - Scale3.min())
 
-z = bias + (weight_1 * TempRange) + (weight_2 * AvgTemp) + (weight_3 * Elevation)
+z = bias + (weight_1 * TempRange) + (weight_2 * AvgTemp) + (weight_4 * Latitude) + (weight_5 * Longitude)
 predict = 1 / (1 + e ** (0-z))
 
 print(predict)
