f9958aba5f0bc6ebd88712759094611ef5e880f5
A Karim <158174701+UnsurpassedRobot@users.noreply.github.com>
2026-06-23 11:11:25 +0100

    Refactor temperature scaling with new data preparation

--- RainChance/RainChanceInput.py
+++ RainChance/RainChanceInput.py
@@ -1,15 +1,15 @@
 from RainChanceTrain import weight_1
 from RainChanceTrain import weight_2
 from RainChanceTrain import bias
-from RainChanceDataPrep import X1
-from RainChanceDataPrep import X1
+from RainChanceDataPrep import Scale1
+from RainChanceDataPrep import Scale2
 from math import e
 
 TempRange = int(input("temperature range: "))
 AvgTemp = int(input("average temperature: "))
 
-TempRange = (TempRange - X1.min())/(X1.max() - X1.min())
-TempRange = (AvgTemp - X2.min())/(X2.max() - X2.min())
+TempRange = (TempRange - Scale1.min())/(Scale1.max() - Scale1.min())
+TempRange = (AvgTemp - Scale2.min())/(Scale2.max() - Scale2.min())
 
 z = bias + (weight_1 * TempRange) + (weight_2 * AvgTemp)
 predict = 1 / (1 + e ** (0-z))
